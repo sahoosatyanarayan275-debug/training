42a263a3a5c03e1cb3d12166478fe5811e475459
sahoosatyanarayan275-debug <sahoosatyanarayan275@gmail.com>
2026-02-22 23:15:27 +0530

    test

---
+++ basic/cube.py
@@ -0,0 +1,5 @@
+#volume of cube
+print("enter side")
+s=float(input())
+v=s*s*s
+print("volume=",v)
---
+++ basic/rectangle.py
@@ -0,0 +1,7 @@
+#perimeter of rectangle
+print("enter length")
+l=int(input())
+print("enter breadth")
+b=int(input())
+pr=2*(l+b)
+print("perimeter=",pr)
